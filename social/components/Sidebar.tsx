01d532e1c6be48eac956f815cd983f010b997b70
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-20 10:38:43 +0530

    sidebar and tailwind css

--- social/components/Sidebar.tsx
+++ social/components/Sidebar.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogout, GoogleLogin } from 'react-google-login'
+import { AiFillHome,AiOutlineMenu } from 'react-icons/ai'
+import{ImCancelCircle} from 'react-icons/im'
+
+
 
 const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(true)
+
+
   return (
-    <div>Sidebar will be here 👍</div>
+    <div >
+      <div className="block xl:hidden m-2 ml-4 mt-3
+     text-xl "
+     onClick={()=>setSidebar((prev)=>!prev)}
+     >
+     
+      {sidebar ? <ImCancelCircle/>:<AiOutlineMenu/>} 
+
+
+      </div>
+
+    </div>
   )
 }
 
